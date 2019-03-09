2335fea7c12db7ac156550b3d0161019a155ea9f
Jacker <hcmlr21@naver.com>
2019-03-09 17:07:04 +0900

    start making Memo App

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by Jkookoo on 09/03/2019.
+//  Copyright © 2019 Jkookoo. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyMemory/MemoFormVC.swift
@@ -0,0 +1,61 @@
+//
+//  MemoFormVC.swift
+//  MyMemory
+//
+//  Created by Jkookoo on 08/03/2019.
+//  Copyright © 2019 Jkookoo. All rights reserved.
+//
+
+import UIKit
+
+class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
+    var subject: String!
+    
+    @IBOutlet var contents: UITextView!
+    @IBOutlet var preview: UIImageView!
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        self.present(picker, animated: false)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.preview.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        
+        picker.dismiss(animated: false)
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let contents = textView.text as NSString
+        let length = ((contents.length > 15) ? 15 : contents.length)
+        self.subject = contents.substring(with: NSRange(location: 0, length: length))
+        
+        self.navigationItem.title = subject
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.contents.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
